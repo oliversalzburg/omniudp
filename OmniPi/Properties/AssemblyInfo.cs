ff005d53dabe1c7d69d7d10ce8c70ec4e0cf3ffa
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-01-29 18:19:06 +0100

    Renaming project...

--- OmniPi/Properties/AssemblyInfo.cs
+++ OmniPi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "OmniPi" )]
+[assembly: AssemblyTitle( "OmniUdp" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "OmniPi" )]
+[assembly: AssemblyProduct( "OmniUdp" )]
 [assembly: AssemblyCopyright( "Copyright ©  2013" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
