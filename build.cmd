4e1018dc3707d57216b6d29f23af53bfcdabdf68
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-06-07 17:29:32 +0200

    Added build script

---
+++ build.cmd
@@ -0,0 +1,24 @@
+@ECHO OFF
+SETLOCAL
+SET SOLUTION_FILENAME=OmniUdp.sln
+IF EXIST packages\NuGet.exe (
+  echo Installing NuGet pacakges...
+  FOR /F %%n in ('dir /b /s packages.config') DO (
+    packages\NuGet.exe install %%n -o packages
+  )
+)
+
+IF NOT EXIST C:\Windows\Microsoft.NET\Framework\v* (
+  ECHO No .NET Framwork found. Go to http://www.microsoft.com/en-us/download/details.aspx?id=30653
+) ELSE (
+  FOR /F %%f in ('dir /B /O-N C:\Windows\Microsoft.NET\Framework\v*') DO (
+    ECHO Using C:\Windows\Microsoft.NET\Framework\%%f
+    CD /D C:\Windows\Microsoft.NET\Framework\%%f
+    GOTO :build
+  )
+  :build
+  ECHO Building...
+  MSBuild.exe %~dp0\%SOLUTION_FILENAME% /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU"
+  
+)
+ENDLOCAL
