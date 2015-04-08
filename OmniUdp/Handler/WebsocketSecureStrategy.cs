a433c6b037111154105782b7827f96c4a2e9d37d
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-04-08 18:38:34 +0200

    [FEATURE] Support password protected x509 certificates/keys

--- OmniUdp/Handler/WebsocketSecureStrategy.cs
+++ OmniUdp/Handler/WebsocketSecureStrategy.cs
@@ -1,29 +1,34 @@
 ﻿using Fleck;
-using OmniUdp.Payload;
 using log4net;
+using OmniUdp.Payload;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 
 namespace OmniUdp.Handler {
-  class WebsocketSecureStrategy : WebsocketStrategy {
-    /// <summary>
-    ///   The logging interface.
-    /// </summary>
-    private readonly ILog Log = LogManager.GetLogger( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType );
+    class WebsocketSecureStrategy : WebsocketStrategy {
+        /// <summary>
+        ///   The logging interface.
+        /// </summary>
+        private readonly ILog Log = LogManager.GetLogger( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType );
 
-    private string CertificateFilename { get; set; }
+        private string CertificateFilename { get; set; }
+        private string CertificatePassword { get; set; }
 
-    public WebsocketSecureStrategy( string ipAddress, JsonFormatter formatter, string certificateFilename, int port = DefaultPort ) : base( ipAddress, formatter, port ) {
-      CertificateFilename = certificateFilename; 
+        public WebsocketSecureStrategy( string ipAddress, JsonFormatter formatter, string certificateFilename, int port = DefaultPort, string certificatePassword = null )
+            : base( ipAddress, formatter, port ) {
+            CertificateFilename = certificateFilename;
+            CertificatePassword = certificatePassword;
+        }
+
+        override public void Initialize() {
+            SocketServer = new WebSocketServer( String.Format( "wss://{0}:{1}", IpAddress, Port ) );
+
+            if( string.IsNullOrEmpty( CertificatePassword ) ) {
+                SocketServer.Certificate = new X509Certificate2( CertificateFilename );
+            } else {
+                SocketServer.Certificate = new X509Certificate2( CertificateFilename, CertificatePassword );
+            }
+            StartServer();
+        }
     }
-
-    override public void Initialize() {
-      SocketServer = new WebSocketServer( String.Format( "wss://{0}:{1}", IpAddress, Port ) );
-      SocketServer.Certificate = new X509Certificate2( CertificateFilename );
-      StartServer();
-    }
-  }
 }
