26bd0420dee48ac9f3dbfae4b491852f4c3f5adf
Oliver Salzburg <oliver.salzburg@gmail.com>
2015-11-22 18:30:16 +0100

    [TASK] Bump version

--- OmniUdp/Properties/AssemblyInfo.cs
+++ OmniUdp/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.17.1540" )]
-[assembly: AssemblyFileVersion( "1.0.17.1540" )]
+[assembly: AssemblyVersion( "1.1.0.0" )]
+[assembly: AssemblyFileVersion( "1.1.0.0" )]
 
 [assembly: log4net.Config.XmlConfigurator( ConfigFileExtension = "log4net", Watch = true )]
